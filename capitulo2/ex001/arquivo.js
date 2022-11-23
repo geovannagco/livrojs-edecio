563d7e394963ee012204f7c3dfffa0ba44c78846
Geovanna Correa de Oliveira <geovanna.gco@hotmail.com>
2022-11-23 11:31:21 -0300

    Exemplo 001

--- capitulo2/ex001/arquivo.js
+++ capitulo2/ex001/arquivo.js
@@ -1,7 +1,14 @@
+/*Programa que exibe o nome informado pelo usuário no campo de edição*/
+//Declara a função mostrarOla
 function mostrarOla() {
+  //obtém o conteúdo do campo (com id=) nome
   var nome = document.getElementById('nome').value;
-  document.getElementById('resposta').innerHTML = 'Olá, ' + nome;
+  //exibe no parágrafo (resposta): "Olá," e o nome informado
+  document.getElementById('resposta').innerHTML = `Olá, ${nome}!`;
 }
+//cria uma referência ao botão (com id=) mostrar
 var mostrar = document.getElementById('mostrar');
-mostrar.onclick = mostrarOla;
+//regista para o botão "mostrar" um ouvinte para o evento click,
+//que ao ser clicado irá chamar a função mostrarOla
+mostrar.addEventListener('click', mostrarOla);
 /*No arquivo Js, as funções devem estar no início do arquivo.*/
