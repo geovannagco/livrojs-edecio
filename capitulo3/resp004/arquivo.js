35306e6c74f8733f0bed6b1f1fe4d1f3b4843565
Geovanna Correa de Oliveira <geovanna.gco@hotmail.com>
2022-12-05 09:52:09 -0300

    Resp.004

---
+++ capitulo3/resp004/arquivo.js
@@ -0,0 +1,45 @@
+function verificaLados() {
+  var inLadoA = document.getElementById('inLadoA');
+  var inLadoB = document.getElementById('inLadoB');
+  var inLadoC = document.getElementById('inLadoC');
+  var outLados = document.getElementById('outLados');
+  var outTipo = document.getElementById('outTipo');
+
+  var ladoA = Number(inLadoA.value);
+  var ladoB = Number(inLadoB.value);
+  var ladoC = Number(inLadoC.value);
+
+  if (
+    ladoA == 0 ||
+    isNaN(ladoA) ||
+    ladoB == 0 ||
+    isNaN(ladoB) ||
+    ladoC == 0 ||
+    isNaN(ladoC)
+  ) {
+    outLados.textContent = `Para continuar preencha os campos com valores válidos.`;
+    return;
+  }
+
+  if (ladoA < ladoB + ladoC || ladoB < ladoA + ladoC || ladoC < ladoA + ladoB) {
+    outLados.textContent = `Podem formar um triângulo`;
+    if (ladoA == ladoB && ladoA == ladoC) {
+      outTipo.textContent = `Tipo: Equilátero`;
+    } else if (ladoA == ladoB || ladoB == ladoC || ladoA == ladoC) {
+      outTipo.textContent = `Tipo: Isósceles`;
+    } else if (
+      (ladoA != ladoB) != ladoC ||
+      (ladoB != ladoC) != ladoA ||
+      (ladoA != ladoC) != ladoB
+    ) {
+      outTipo.textContent = `Tipo: Escaleno`;
+    }
+    return;
+  } else {
+    outTipo.textContent = `Impossivel formar um triângulo`;
+  }
+}
+
+var btVerificar = document.getElementById('btVerificar');
+
+btVerificar.addEventListener('click', verificaLados);
