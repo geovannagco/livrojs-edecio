15feacd674e1c4ba37af62c2713d051c889a324e
Geovanna Correa de Oliveira <geovanna.gco@hotmail.com>
2022-12-07 10:38:57 -0300

    resp004

--- capitulo4/js/resp004.js
+++ capitulo4/js/resp004.js
@@ -1,32 +1,35 @@
-/*d) Elaborar um programa que leia um número e exiba estrelas na página, em linhas diferentes. A cada nova linha, o número de estrelas deve ser incrementado. Observação: caso você informe um valor alto, as linhas podem ultrapassar o tamanho da imagem e se alinhar à margem esquerda da página. Para evitar que isso ocorra, crie uma nova classe no arquivo estilos.css, que de½na uma ¾utuação à direita para essa imagem. A Figura 4.18 ilustra uma execução do programa desse exercício. */
+/* d) Elaborar um programa que leia um número e exiba estrelas na página, em linhas diferentes. A cada nova linha, o número de estrelas deve ser incrementado. */
 function fabricarEstrelas() {
-  //cria referência aos elementos html
+  //cria referência aos elementos html manipulados pela function
   var inNumero = document.getElementById('inNumero');
   var outEstrelas = document.getElementById('outEstrelas');
 
-  //converte o conteúdo do campo inNumero
+  //obtém o conteúdo do campo inNumero
   var numero = Number(inNumero.value);
 
   //verifica se o campo inNumero foi preenchido corretamente
-  if (numero == 0 || isNaN(numero)) {
-    alert('Informe um número válido');
+  if (numero == 0 || numero == '' || isNaN(numero)) {
+    alert('Preencha o número de linhas');
     inNumero.focus();
     return;
   }
 
-  //cria uma variável do tipo string, que irá concatenar a resposta
-  var resposta = '';
+  //cria a variável que irá compor o laço de repetição
+  var estrelas = '';
 
-  //cria um laço de repetição
+  //inicia o laço de repetição 'for'
+  //o 1º 'for' é usado para icrementar o nº de linhas
+  //o 2º 'for' é usado para icrementar o nº de '*' a cada linha
   for (var i = 1; i <= numero; i++) {
-    //a variávl resposta vai acumulando os novos conteúdos
-    resposta = resposta + '* \n';
+    for (var j = 1; j <= i; j++) {
+      estrelas += '*';
+    }
+    estrelas += '\n';
   }
-
-  //o conteúdo da tag pre é alterado para exibição das linhas
-  outEstrelas.textContent = resposta;
+  outEstrelas.textContent = estrelas;
 }
 
-//cria referência ao botão e após associa function ao evento click
+//referencia o elemento btCriar e associa a function ao evento click
+
 var btCriar = document.getElementById('btCriar');
 btCriar.addEventListener('click', fabricarEstrelas);
